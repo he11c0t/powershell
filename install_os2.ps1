ac66a8a08f5f468c242f055b6fbbc333e9140415
Ilya <81704290+he11c0t@users.noreply.github.com>
2024-07-08 20:22:08 +0300

    Add files via upload

---
+++ install_os2.ps1
@@ -0,0 +1,12 @@
+﻿$computerName = ""
+$isoPath = "C:\1\Windows_10_22h2.iso"
+Get-Service -ComputerName $computerName -Name WinRM -ErrorAction Stop | Set-Service -StartupType Manual -Status Running
+# Монтируем ISO-образ на удаленном компьютере
+Invoke-Command -ComputerName $computerName -ArgumentList $isoPath -ScriptBlock {
+    param($isoPath)
+    $driveLetter = Mount-DiskImage -ImagePath $isoPath -PassThru | Get-Volume | Where-Object { $_.DriveType -eq "CD-ROM" } | Select-Object -ExpandProperty DriveLetter
+
+    # Запускаем процесс установки Windows на удаленном компьютере с использованием монтированного образа
+    $setupExePath = "${driveLetter}:\setup.exe"
+    Start-Process -FilePath $setupExePath -ArgumentList "/auto", "upgrade", "/quiet", "/noreboot" -Wait
+}
