266cf64c1f96429c497ef281cab3acd12b4ac03e
Ilya <81704290+he11c0t@users.noreply.github.com>
2024-07-08 20:24:57 +0300

    Add files via upload

---
+++ bitlocker check.ps1
@@ -0,0 +1,7 @@
+﻿do {
+    $computerName = Read-Host "Введите имя компьютера"
+    Get-ADObject -SearchBase (Get-AdComputer $computerName).DistinguishedName -Filter {objectclass -eq 'msFVE-RecoveryInformation'} -Properties msfve-recoverypassword | fl name, msfve-recoverypassword
+    # Остановка скрипта для просмотра результатов
+    Read-Host "Нажмите Enter, чтобы продолжить"
+}
+while ($true)
