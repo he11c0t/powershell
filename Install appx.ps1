c20bcf143fab471a1afeceb727f999626669c94a
Ilya <81704290+he11c0t@users.noreply.github.com>
2024-07-08 20:25:49 +0300

    Add files via upload

---
+++ Install appx.ps1
@@ -0,0 +1,24 @@
+﻿$computers = Get-Content "C:\list\test.txt"
+$appxPackages = Get-ChildItem -Path "C:\1\appx\"
+
+
+foreach ($computer in $computers) {
+    Get-Service -ComputerName $computer -Name WinRM -ErrorAction Stop | Set-Service -StartupType Manual -Status Running
+    $session = New-PSSession -ComputerName $computer
+    Try {
+        if (-not (Test-Path "\\$computer\C$\temp")) {
+            New-Item -Path "\\$computer\C$" -Name 'temp' -ItemType Directory -ErrorAction Stop | Out-Null
+        }
+    } Catch {
+        Write-Host "Не удалось создать папку C:\temp на компьютере $computer"
+        continue
+    }
+    foreach ($appxPackage in $appxPackages) {
+        Copy-Item -Path $appxPackage.FullName -Destination "\\$computer\C$\temp\$appxPackage" -Force
+        Invoke-Command -ComputerName $computer -ScriptBlock {
+            Add-AppxProvisionedPackage -Online -PackagePath "C:\temp\$($using:appxPackage.Name)" -SkipLicense
+        }
+        del "\\$computer\C$\temp\$appxPackage"
+    }
+    Remove-PSSession $session
+}
